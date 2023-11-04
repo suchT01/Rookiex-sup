5e4b1a0ac0072f1a92aacc29c533ced6a13f8878
suchT01 <zorrillaja30@gmail.com>
2023-11-03 21:30:36 -0400

    bof

--- rookiex/src/App.jsx
+++ rookiex/src/App.jsx
@@ -9,16 +9,7 @@ function App() {
   return (
 
     <div>
-<<<<<<< HEAD
       <Index/>
-=======
-      {headerCatalogo()}
-      
-      <div>
-        {quienesSomos()}
-      </div>
-      
->>>>>>> df00fd77b07e7cf26c53068099603b7d31577cde
     </div>
     
     
