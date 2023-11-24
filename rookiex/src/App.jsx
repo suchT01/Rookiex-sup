b57bccde8b88dbf8d72ab41439309806ecdfbde3
aledil0807 <aledil0807@gmail.com>
2023-11-23 22:08:54 -0400

    asdad

--- rookiex/src/App.jsx
+++ rookiex/src/App.jsx
@@ -4,12 +4,69 @@ import { HeaderCatalogo } from './components/header'
 import { renderCatalogo, renderNoResults } from './components/Catalogo'
 import responseProducts from './mocks/with-results.json'
 
+function useSearch(){
+  const [query, setQuery] = useState('')
+  const [error, setError] = useState(null)
+  const isFirstInput = useRef(true)
 
+  useEffect(() => {
+    
+    if(isFirstInput.current){
+      isFirstInput.current = query == ''
+      return
+    }
+
+    if (query == ''){
+      setError('No se puede buscar una pelicula vacía')
+      return
+    }
+
+    if (query.match(/^\d*$/)){
+      setError('No se puede buscar una pelicula con un numero')
+      return
+    }
+
+    if (query.length < 3){
+      setError('La busqueda debe contener al menos tres caracteres')
+      return
+    }
+
+    setError(null)
+
+  }, [query])
+
+  return{query, setQuery, error}
+
+}
 
 function App() {
 
+  const { query, setQuery, error} = useSearch()
+
   const [responseproducts, setResponsiveproducts] = useState([])
   const products = responseProducts.Search
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    
+    if(previousSearch.current == query) return
+    if(query){
+      searchProducts({query,setResponsiveproducts}) // Crear funcion searchProducts
+      
+    }
+    else{
+      setResponsiveproducts(withoutResults)
+    }
+    
+  }
+
+  const handleChange = (event) => {
+    const newQuery = event.target.value
+    setQuery(newQuery)
+    
+  }
+
 
 
   const hasproducts = products?.length >0
