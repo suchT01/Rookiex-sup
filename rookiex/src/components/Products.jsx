6312ab4058f286523745968c13ce821368d888d2
Johiner Almeida <ajohiner.07@gmail.com>
2024-01-17 01:23:28 -0400

    kk

--- rookiex/src/components/Products.jsx
+++ rookiex/src/components/Products.jsx
@@ -12,7 +12,7 @@ export function Products ({ products }) {
   return (
     <main className='products'>
       <ul>
-        {products.slice(0, 15).map(product => {
+        {products.slice(0, 16).map(product => {
           const isProductInCart = checkProductInCart(product)
 
           return (
